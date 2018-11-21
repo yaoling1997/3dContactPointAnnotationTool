71bb527eb4708e17de74ce9ef05fb480f1883f30
yaoling1997 <379145124@qq.com>
2018-11-21 10:35:18 +0800

    day14

---
+++ 3dContactPointAnnotationTool/Assets/scripts/Button/ButtonOpenImageOnClick.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class ButtonOpenImageOnClick : MonoBehaviour
+
+{
+    public InputField inputField;
+    public void OnClick()
+
+    {
+        OpenFileName ofn = new OpenFileName();
+        ofn.structSize = Marshal.SizeOf(ofn);
+        ofn.filter = "图片文件(*.jpg*.png)\0*.jpg;*.png";
+        ofn.file = new string(new char[256]);
+        ofn.maxFile = ofn.file.Length;
+        ofn.fileTitle = new string(new char[64]);
+        ofn.maxFileTitle = ofn.fileTitle.Length;
+        ofn.initialDir = Application.streamingAssetsPath.Replace('/', '\\');//默认路径
+        ofn.title = "窗口标题";
+        ofn.flags = 0x00080000 | 0x00001000 | 0x00000800 | 0x00000008;
+        if (LocalDialog.GetOpenFileName(ofn)) {
+            Debug.Log("file: " + ofn.file);
+            inputField.text = ofn.file;
+        }
+    }
+}
