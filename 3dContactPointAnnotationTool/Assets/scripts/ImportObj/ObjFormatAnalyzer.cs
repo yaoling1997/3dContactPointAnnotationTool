02140bb79260ace838b98b6477dcd9cb9d63a7df
yaoling1997 <379145124@qq.com>
2018-12-27 17:29:19 +0800

    day24 obj适配

--- 3dContactPointAnnotationTool/Assets/scripts/ImportObj/ObjFormatAnalyzer.cs
+++ 3dContactPointAnnotationTool/Assets/scripts/ImportObj/ObjFormatAnalyzer.cs
@@ -32,8 +32,8 @@ namespace Hont
         public Vector[] VertexNormalArr;
         public Vector[] VertexTextureArr;
         public Face[] FaceArr;
-        public int[] ObjFaceBegin;
-        public string[] ObjName;
+        public int[] ObjFaceBeginArr;
+        public string[] ObjNameArr;
 
         public void Analyze(string content)
         {
@@ -46,6 +46,9 @@ namespace Hont
             var faceList = new List<Face>();
             var objFaceBeginList = new List<int>();
             var objNameList = new List<string>();
+            string objName = "";//物体的名称
+            int defaultObjNameNum = 0;//默认物体名称编号
+            int dealtFaceNum = 0;//已处理的面数
 
             for (int i = 0; i < lines.Length; i++)
             {
@@ -66,7 +69,7 @@ namespace Hont
                 else if (currentLine.Contains("vt "))
                 {
                     var splitInfo = currentLine.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-                    vertexTextureList.Add(new Vector() { X = float.Parse(splitInfo[1]), Y = float.Parse(splitInfo[2]), Z = float.Parse(splitInfo[3]) });
+                    vertexTextureList.Add(new Vector() { X = float.Parse(splitInfo[1]), Y = float.Parse(splitInfo[2]), Z = 3 < splitInfo.Length ? float.Parse(splitInfo[3]):0 });
                 }
                 else if (currentLine.Contains("vn "))
                 {
@@ -83,32 +86,59 @@ namespace Hont
                     var face4 = isQuad ? splitInfo[4].Split('/') : null;
                     var face = new Face();
                     face.Points = new FacePoint[4];
-                    face.Points[0] = new FacePoint() { VertexIndex = int.Parse(face1[0]), TextureIndex = int.Parse(face1[1]), NormalIndex = int.Parse(face1[2]) };
-                    face.Points[1] = new FacePoint() { VertexIndex = int.Parse(face2[0]), TextureIndex = int.Parse(face2[1]), NormalIndex = int.Parse(face2[2]) };
-                    face.Points[2] = new FacePoint() { VertexIndex = int.Parse(face3[0]), TextureIndex = int.Parse(face3[1]), NormalIndex = int.Parse(face3[2]) };
+                    face.Points[0] = new FacePoint() { VertexIndex = int.Parse(face1[0]), TextureIndex = 1 < face1.Length?int.Parse(face1[1]):0, NormalIndex = 2 < face1.Length ? int.Parse(face1[2]):0 };
+                    face.Points[1] = new FacePoint() { VertexIndex = int.Parse(face2[0]), TextureIndex = 1 < face2.Length?int.Parse(face2[1]):0, NormalIndex = 2 < face2.Length ? int.Parse(face2[2]):0 };
+                    face.Points[2] = new FacePoint() { VertexIndex = int.Parse(face3[0]), TextureIndex = 1 < face3.Length?int.Parse(face3[1]):0, NormalIndex = 2 < face3.Length ? int.Parse(face3[2]):0 };
                     face.Points[3] = isQuad ? new FacePoint() { VertexIndex = int.Parse(face4[0]), TextureIndex = int.Parse(face4[1]), NormalIndex = int.Parse(face4[2]) } : default(FacePoint);
                     face.IsQuad = isQuad;
 
                     faceList.Add(face);
                 }
                 else if (currentLine.Contains("o ")) {
-                    string objName = "";
+                    if (dealtFaceNum < faceList.Count) {
+                        objName = "default" + defaultObjNameNum;
+                        defaultObjNameNum++;
+                        objFaceBeginList.Add(dealtFaceNum);
+                        objNameList.Add(objName);
+                        dealtFaceNum = faceList.Count;
+                    }
+                    objName = "";
                     int p = currentLine.IndexOf('o');
                     int length = currentLine.Length;
                     for (p++; currentLine[p] == ' '; p++) ;
                     for (; p < length; p++)
                         objName += currentLine[p];
-                    objFaceBeginList.Add(faceList.Count);
-                    objNameList.Add(objName);
+                    if (objFaceBeginList.Count > 0 && dealtFaceNum == objFaceBeginList[objFaceBeginList.Count - 1]) {//前一个对象没有对应的面，直接改名字
+                        objNameList[objNameList.Count - 1] = objName;
+                    }
+                    else
+                    {
+                        objFaceBeginList.Add(dealtFaceNum);
+                        objNameList.Add(objName);
+                    }
                 }
+            }
+
+            if (dealtFaceNum < faceList.Count)//还有剩余的面没有被处理
+            {
+                objName = "default" + defaultObjNameNum;
+                defaultObjNameNum++;
+                objFaceBeginList.Add(dealtFaceNum);
+                objNameList.Add(objName);
+                dealtFaceNum = faceList.Count;
+            }
+            if (objFaceBeginList.Count > 0 && dealtFaceNum == objFaceBeginList[objFaceBeginList.Count - 1])
+            {//最后一个对象没有对应的面，删除
+                objFaceBeginList.RemoveAt(objFaceBeginList.Count-1);
+                objNameList.RemoveAt(objNameList.Count-1);
             }
 
             VertexArr = vertexList.ToArray();
             VertexNormalArr = vertexNormalList.ToArray();
             VertexTextureArr = vertexTextureList.ToArray();
             FaceArr = faceList.ToArray();
-            ObjFaceBegin = objFaceBeginList.ToArray();
-            ObjName = objNameList.ToArray();
+            ObjFaceBeginArr = objFaceBeginList.ToArray();
+            ObjNameArr = objNameList.ToArray();
         }
     }
 }
--- 3dContactPointAnnotationTool/Assets/scripts/ImportObj/ObjFormatAnalyzerFactory.cs
+++ 3dContactPointAnnotationTool/Assets/scripts/ImportObj/ObjFormatAnalyzerFactory.cs
@@ -15,7 +15,7 @@ namespace Hont
             var objFormatAnalyzer = new ObjFormatAnalyzer();
 
             objFormatAnalyzer.Analyze(File.ReadAllText(objFilePath));
-            int length = objFormatAnalyzer.ObjFaceBegin.Length;
+            int length = objFormatAnalyzer.ObjFaceBeginArr.Length;//有多少个3d模型
             var re = new List<GameObject>();
             var sourceVertexArr = objFormatAnalyzer.VertexArr;
             var sourceVertexNormalArr = objFormatAnalyzer.VertexNormalArr;
@@ -25,8 +25,8 @@ namespace Hont
             for (int objId = 0; objId < length; objId++)
             {
                 var go = new GameObject();
-                go.name = objFormatAnalyzer.ObjName[objId];
-                var skinnedMeshRenderer = go.AddComponent<SkinnedMeshRenderer>();
+                go.name = objFormatAnalyzer.ObjNameArr[objId];
+                var skinnedMeshRenderer = go.AddComponent<SkinnedMeshRenderer>();//添加skinnedMeshRenderer
 
                 var mesh = new Mesh();
 
@@ -34,10 +34,10 @@ namespace Hont
                 var vertexNormalList = new List<Vector3>();
                 var uvList = new List<Vector2>();
 
-                int faceBeginId = objFormatAnalyzer.ObjFaceBegin[objId];
+                int faceBeginId = objFormatAnalyzer.ObjFaceBeginArr[objId];
                 int faceEndId = faceArr.Length;//左闭右开
                 if (objId < length - 1)
-                    faceEndId = objFormatAnalyzer.ObjFaceBegin[objId + 1];
+                    faceEndId = objFormatAnalyzer.ObjFaceBeginArr[objId + 1];
                 int triangleNum = 0;
                 for (int i = faceBeginId; i < faceEndId; i++)
                     if (faceArr[i].IsQuad)
@@ -47,31 +47,24 @@ namespace Hont
                 for (int i = faceBeginId, j = 0; i < faceEndId; i++)
                 {
                     var currentFace = faceArr[i];
-
+                    var defaultVec = default(ObjFormatAnalyzer.Vector);
                     triangles[j] = j;
                     triangles[j + 1] = j + 1;
                     triangles[j + 2] = j + 2;
-
-                    var vec = sourceVertexArr[currentFace.Points[0].VertexIndex - 1];
-                    vertexList.Add(new Vector3(vec.X, vec.Y, vec.Z));
-                    vec = sourceVertexNormalArr[currentFace.Points[0].VertexIndex - 1];
-                    vertexNormalList.Add(new Vector3(vec.X, vec.Y, vec.Z));
-                    var uv = sourceUVArr[currentFace.Points[0].TextureIndex - 1];
-                    uvList.Add(new Vector2(uv.X, uv.Y));
-
-                    vec = sourceVertexArr[currentFace.Points[1].VertexIndex - 1];
-                    vertexList.Add(new Vector3(vec.X, vec.Y, vec.Z));
-                    vec = sourceVertexNormalArr[currentFace.Points[1].VertexIndex - 1];
-                    vertexNormalList.Add(new Vector3(vec.X, vec.Y, vec.Z));
-                    uv = sourceUVArr[currentFace.Points[1].TextureIndex - 1];
-                    uvList.Add(new Vector2(uv.X, uv.Y));
-
-                    vec = sourceVertexArr[currentFace.Points[2].VertexIndex - 1];
-                    vertexList.Add(new Vector3(vec.X, vec.Y, vec.Z));
-                    vec = sourceVertexNormalArr[currentFace.Points[2].VertexIndex - 1];
-                    vertexNormalList.Add(new Vector3(vec.X, vec.Y, vec.Z));
-                    uv = sourceUVArr[currentFace.Points[2].TextureIndex - 1];
-                    uvList.Add(new Vector2(uv.X, uv.Y));
+                    for (int k = 0; k < 3; k++)//0,1,2是一个三角形
+                    {
+                        var vId = currentFace.Points[k].VertexIndex - 1;
+                        var nId = currentFace.Points[k].NormalIndex - 1;
+                        var tId = currentFace.Points[k].TextureIndex - 1;
+                        var vec = sourceVertexArr[vId];
+                        vertexList.Add(new Vector3(vec.X, vec.Y, vec.Z));
+                        Debug.Log("sourceVertexNormalArr.Length:" + sourceVertexNormalArr.Length);
+                        Debug.Log("nId:" + nId);
+                        vec = nId!=-1?sourceVertexNormalArr[nId]: defaultVec;
+                        vertexNormalList.Add(new Vector3(vec.X, vec.Y, vec.Z));
+                        var uv = tId!=-1? sourceUVArr[tId] : defaultVec;
+                        uvList.Add(new Vector2(uv.X, uv.Y));
+                    }
 
                     if (currentFace.IsQuad)
                     {
@@ -79,35 +72,28 @@ namespace Hont
                         triangles[j + 4] = j + 4;
                         triangles[j + 5] = j + 5;
                         j += 3;
-
-                        vec = sourceVertexArr[currentFace.Points[0].VertexIndex - 1];
-                        vertexList.Add(new Vector3(vec.X, vec.Y, vec.Z));
-                        vec = sourceVertexNormalArr[currentFace.Points[0].VertexIndex - 1];
-                        vertexNormalList.Add(new Vector3(vec.X, vec.Y, vec.Z));
-                        uv = sourceUVArr[currentFace.Points[0].TextureIndex - 1];
-                        uvList.Add(new Vector2(uv.X, uv.Y));
-
-                        vec = sourceVertexArr[currentFace.Points[2].VertexIndex - 1];
-                        vertexList.Add(new Vector3(vec.X, vec.Y, vec.Z));
-                        vec = sourceVertexNormalArr[currentFace.Points[2].VertexIndex - 1];
-                        vertexNormalList.Add(new Vector3(vec.X, vec.Y, vec.Z));
-                        uv = sourceUVArr[currentFace.Points[2].TextureIndex - 1];
-                        uvList.Add(new Vector2(uv.X, uv.Y));
-
-                        vec = sourceVertexArr[currentFace.Points[3].VertexIndex - 1];
-                        vertexList.Add(new Vector3(vec.X, vec.Y, vec.Z));
-                        vec = sourceVertexNormalArr[currentFace.Points[3].VertexIndex - 1];
-                        vertexNormalList.Add(new Vector3(vec.X, vec.Y, vec.Z));
-                        uv = sourceUVArr[currentFace.Points[3].TextureIndex - 1];
-                        uvList.Add(new Vector2(uv.X, uv.Y));
+                        for (int k = 0; k < 4; k++)//0,2,3是一个三角形
+                        {
+                            if (k == 1)
+                                continue;
+                            var vId = currentFace.Points[k].VertexIndex - 1;
+                            var nId = currentFace.Points[k].NormalIndex - 1;
+                            var tId = currentFace.Points[k].TextureIndex - 1;
+                            var vec = sourceVertexArr[vId];
+                            vertexList.Add(new Vector3(vec.X, vec.Y, vec.Z));
+                            vec = nId < sourceVertexNormalArr.Length ? sourceVertexNormalArr[nId] : defaultVec;
+                            vertexNormalList.Add(new Vector3(vec.X, vec.Y, vec.Z));
+                            var uv = tId < sourceUVArr.Length ? sourceUVArr[tId] : defaultVec;
+                            uvList.Add(new Vector2(uv.X, uv.Y));
+                        }
                     }
 
                     j += 3;
                 }
 
                 mesh.vertices = vertexList.ToArray();
-                mesh.normals = vertexNormalList.ToArray();
-                mesh.uv = uvList.ToArray();
+                mesh.normals = sourceVertexNormalArr.Length==0 ? null:vertexNormalList.ToArray();
+                mesh.uv = sourceUVArr.Length==0 ? null:uvList.ToArray();
                 mesh.triangles = triangles;
 
                 skinnedMeshRenderer.sharedMesh = mesh;
